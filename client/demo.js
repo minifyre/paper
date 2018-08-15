8629f730df8ed985067ec5187afae7cdc8bb6acf
Minifyre <minifyre@gmail.com>
2018-08-15 15:38:38 -0500

    +demo files
    Added demo files to restore working demo while working on revamping core functionality.

---
+++ client/demo.js
@@ -0,0 +1,194 @@
+'use strict';
+import {chant} from '../js/chant/chant.mjs';
+import {paper} from './paper.js';
+const
+state=chant(),
+input={},
+logic={},
+output={};
+input.updatePic=function(action)//@todo actions don't have id props...
+{
+	output.renderPic();
+};
+input.updateTxt=function(action)
+{
+	action.device!==state.get('private.id')?output.renderCode(action.val):
+											output.renderBrowser(action.val);
+};
+input.updateVid=function(action)
+{
+	action.device!==state.get('private.id')?output.renderVid(action.val):'';
+};
+input.eventHandlers=function()
+{
+	q('textarea').on('input',input.txt);
+	q('canvas').on('pointerdown',input.drawing);
+};
+input.drawing=function(evt)
+{
+	const
+	target=q(evt.target),
+	el=target.norm(),
+	drawPt=function(evt)
+	{
+		const
+		can=
+		{
+			h:el.offsetHeight,
+			w:el.offsetWidth,
+			x:el.offsetLeft,
+			y:el.offsetTop
+		},
+		img=
+		{
+			h:el.height,
+			w:el.width
+		},
+		x=Math.round((evt.pageX-can.x)*(img.w/can.w)),
+		y=Math.round((evt.pageY-can.y)*(img.h/can.h));
+		console.log(can,evt);
+		state.set('public.files.pic.pts.'+x+'x'+y,1);
+	},
+	cleanup=function()
+	{
+		target
+		.off('pointerdown,pointermove',drawPt)
+		.off('pointerup',cleanup)
+		.on('pointerdown',input.drawing);
+	};
+	target
+	.off('pointerdown',input.drawing)
+	.on('pointerdown,pointermove',drawPt)
+	.on('pointerup',cleanup);
+	drawPt(evt);
+};
+input.init=async function()
+{
+	await state.with();//@todo add error hander
+	const
+	keys=state.keys('public.files'),
+	no=x=>!keys.includes(x);
+	no('txt')?state.set('public.files.txt',{data:''}):'';
+	no('pic')?state.set('public.files.pic',{pts:{}}):'';
+	no('vid')?state.set('public.files.vid',
+	{
+		id:'PUv66718DII',
+		time:0.556959
+	}):'';
+	//+state listeners
+	state.on({path:'public.files.pic',type:'set',func:input.updatePic});
+	state.on({path:'public.files.txt',type:'set',func:input.updateTxt});
+	state.on({path:'public.files.vid',type:'set',func:input.updateVid});
+	//setup panes
+	q('main').last
+	(
+		output.paneTxt(),
+		output.paneBrowser(),
+		output.paneVid(),
+		output.panePic()
+	);		
+	//inital renders
+	output.renderCode(state.get('public.files.txt.data'));
+	output.renderPic();
+	output.initVideoPlayer();
+	input.eventHandlers();
+};
+input.txt=function(evt)
+{
+	const val=q(evt.target).val();
+	state.set('public.files.txt.data',val);
+};
+//{app,id,src}pane.output.paneDOM(json)
+output.paneBrowser=()=>q.create('iframe',{class:'pane',name:'browser'});
+output.panePic=()=>q.create('canvas',{class:'pane'});
+output.paneTxt=()=>q.create('textarea',{class:'pane'});
+output.paneVid=()=>q.create('div',{class:'pane',id:'player'});
+output.renderCode=function(txt)
+{
+	output.renderEditor(txt);
+	output.renderBrowser(txt);
+};
+output.renderEditor=function(txt)
+{
+	q('textarea').val(txt);
+	//.html(txt);//@todo did html preserve the cursor position?
+};
+output.renderBrowser=function(txt)
+{
+	const 
+	el=q('iframe'),
+	doc=el.norm('contentWindow').document;
+	//open('','browser');
+	doc.open('text/html');
+	doc.write(txt);
+	doc.close();
+};
+output.renderPic=function()
+{
+	const
+	el=q('canvas').norm(),
+	ctx=el.getContext('2d'),
+	{height,width}=el;
+	ctx.clearRect(0,0,width,height);
+	state.keys('public.files.pic.pts')
+	.forEach(function(pt)
+	{
+		const [x,y]=pt.split('x');
+		ctx.fillRect(x,y,1,1);
+	});
+};
+//youtube player
+output.initVideoPlayer=function()
+{
+	const
+	url='https://www.youtube.com/iframe_api',
+	tag=q.create('script').attr('src',url).norm();
+	q('script').prev(tag);
+	var
+	player,
+	setup=false;
+	output.renderVid=function(evt)
+	{
+		const {id,time}=state.get('public.files.vid');
+		player.loadVideoById(id,time);
+		player.pauseVideo();
+	};
+	//@todo find a way to not pollute global scope
+	window.onYouTubeIframeAPIReady=function()
+	{
+		player=new YT.Player('player',
+		{
+			height:'390',
+			width:'640',
+			videoId:state.get('public.files.vid.id'),
+			/*playerVars:
+			{
+				listType:'playlist',
+				list:'PLjhH01A4YOO3h0LwGNTf2F37L0_Gn7O4D',
+			},*/
+			events:
+			{
+				onReady:function()
+				{
+					if (!setup)
+					{
+						output.renderVid();
+						setup=true;
+					}
+				},
+				onStateChange:function(evt)
+				{
+					if (evt.data===YT.PlayerState.PAUSED)
+					{
+						state.set('public.files.vid',
+						{
+							id:player.getVideoData().video_id,
+							time:player.getCurrentTime()
+						});
+					}
+				}
+			}
+		});
+	};
+};
+onload=input.init;
