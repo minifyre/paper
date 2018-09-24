76106c9dda5635053c6022333aa3cfeb26ac071c
Minifyre <minifyre@gmail.com>
2018-09-24 17:40:12 -0500

    generated dom from state
    Created tabbed-editors & other custom elements by going through state.panes.

--- client/canary.mjs
+++ client/canary.mjs
@@ -5,16 +5,26 @@ import youtube from './node_modules/youtube-viewer/index.mjs'
 const apps={code,tabbed}
 onload=async function()
 {
+	// const {error,value}=await fetch('./node_modules/')
+	// .then(x=>x.text())
+	// .then(value=>({value}))
+	// .catch(error=>{error})
+	// if(error) return console.error(error)
+// 	<tabbed-editor class=pane>
+// 	<code-editor></code-editor>
+// </tabbed-editor>
+// <iframe class=pane></iframe>
+// <youtube-player class=pane></youtube-player>
 	await Promise.all([code,tabbed,youtube].map(fn=>fn()))//init custom-els
 	const
 	read=
 	{
 		files:{},
-//panes:{window:apps.pane.logic({id:'window'})},
+		panes:{},
 		tabs:{}
 	},
-	[browser,tabs,text]=['iframe.pane','tabbed-editor','code-editor']
-						.map(x=>document.querySelector(x)),
+	// [browser,tabs,text]=['iframe.pane','tabbed-editor','code-editor']
+	// 					.map(x=>document.querySelector(x)),
 	renderView=function({type,path,val})
 	{
 		if(type==='get') return			
@@ -29,27 +39,59 @@ onload=async function()
 	},
 	//@todo +pre op to set target=el or server that the change came from
 	state=truth(read,renderView)
-	tabs.addEventListener('tab',function({detail,target})
+	if(!Object.keys(state.panes).length)
 	{
 		const
-		{close,open}=detail,
-		child=target.querySelector(':scope>*')
-		if(close)
+		//@todo add tabs to panes
+		pane={height:50,id:'sub-pane',type:'app-viewer',width:50,x:0,y:0},
+		mkPane=x=>Object.assign({},pane,x)
+		Object.assign(state.panes,
 		{
-			delete state.files[close]//@todo preserve if file has a value
-			delete state.tabs[close]
-		}
-		if(open)
-		{
-			if(!state.files[open])
-			{
-				state.files[open]={id:open,value:''}
-				//@todo get default view from child (sans value)
-				state.tabs[open]={id:open,view:{}}
-			}
-			const [{value},view]=['files','tabs'].map(x=>state[x][open])
-			child.state=Object.assign({},{value},view)
-		}
+			code:mkPane({id:'code',tabs:[{id:'code',name:'index.html'}],type:'code-editor'}),
+			iframe:mkPane({id:'iframe',tabs:[{id:'code',name:'index.html'}],type:'iframe-viewer'}),
+			youtube:mkPane({id:'youtube',tabs:[{id:'vide',name:'video'}],type:'youtube-viewer'}),
+			pixel:mkPane({id:'pixel',tabs:[{id:'pic',name:'pic.png'}],type:'pixel-editor'})
+		})
+	}
+	Object.values(state.panes)
+	.forEach(function(pane)
+	{
+		const
+		tabs=Object.assign(new tabbed.editor(pane),{className:'pane'}),
+		child=document.createElement(pane.type)
+		tabs.append(child)
+		document.body.append(tabs)
 	})
-	text.addEventListener('input',({target})=>state.files[tabs.tab].value=target.value)
+	//@todo get code from server
+	//@todo if files & tabs are empty, set default files
+	// if(!state.files.code)
+	// {
+	// 	//@todo make file creator function
+	// 	state.files.code={id:'code',path:'',value:'<!Doctype html>\n<h1>Hello!</h1>'}
+		
+	// }
+	//@tod load tabs with tabs in state
+	// tabs.addEventListener('tab',function({detail,target})
+	// {
+	// 	const
+	// 	{close,open}=detail,
+	// 	child=target.querySelector(':scope>*')
+	// 	if(close)
+	// 	{
+	// 		delete state.files[close]//@todo preserve if file has a value
+	// 		delete state.tabs[close]
+	// 	}
+	// 	if(open)
+	// 	{
+	// 		if(!state.files[open])
+	// 		{
+	// 			state.files[open]={id:open,value:''}
+	// 			//@todo get default view from child (sans value)
+	// 			state.tabs[open]={id:open,view:{}}
+	// 		}
+	// 		const [{value},view]=['files','tabs'].map(x=>state[x][open])
+	// 		child.state=Object.assign({},{value},view)
+	// 	}
+	// })
+	// text.addEventListener('input',({target})=>state.files[tabs.tab].value=target.value)
 }
