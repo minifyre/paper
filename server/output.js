f21e6ce6b931898c4947195cf1f291eb04092e65
Minifyre <minifyre@gmail.com>
2018-10-13 14:55:35 -0500

    simplified

--- server/output.js
+++ server/output.js
@@ -2,30 +2,36 @@
 const
 fs=require('fs'),
 os=require('os'),
-{config,logic,util}=require('./logic.js'),
+silo=require('./logic.js'),
+{config,logic,util}=silo,
 output={}
+
 output.dir=path=>util.callback2promise(fs.readdir,path)
 output.file=path=>util.callback2promise(fs.readFile,path)
 output.index=async function(path)
 {
-	const arr=await output.dir(path.replace(/index\.html$/,''))
-	return `<ul>${arr
-	.sort()
-	.map(file=>`<li><a href="${file}">${file}</a></li>`)
-	.join('')}</ul>`
+	const
+	arr=await output.dir(path.replace(/index\.html$/,'')),
+	items=	arr
+			.sort()
+			.map(file=>`<li><a href="${file}">${file}</a></li>`)
+			.join('')
+
+	return `<ul>${items}</ul>`
 }
+
 output.ip=function(interfaces=os.networkInterfaces())
 {
 	return	Object.values(interfaces)
 			.map(x=>Object.values(x))
-			.reduce((arr,x)=>arr.concat(x),[])//flatten array
+			.reduce((arr,x)=>[...arr,...x],[])//flatten array
 			.filter(({family,internal})=>family==='IPv4'&&!internal)
 			.map(({address})=>address)[0]
 }
 output.response=function(res,opts)
-{
+{//@todo simplify next line with util.mk?
 	const {code,data,encoding,type}=Object.assign({},config.response,opts)
 	res.writeHead(code,{'Content-Type':type})
 	res.end(data,encoding)
 }
-module.exports={config,logic,output,util}
+module.exports=Object.assign(silo,{output})
