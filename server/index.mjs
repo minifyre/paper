09b59ce1f30fe5c11ef08f63b937963b64e91a6b
Minifyre <minifyre@gmail.com>
2018-10-18 14:46:42 -0500

    simplified server code

--- server/index.mjs
+++ server/index.mjs
@@ -1,17 +1,17 @@
-import chant from '../node_modules/chant/chant.server.js'
+import chant from '../node_modules/chant/server.mjs'
 import silo from './input.mjs'
 import truth from '../node_modules/truth/truth.mjs'
 
 const 
 {config,input,logic,output,util}=silo,
-//truth=await import('../node_modules/truth/truth.mjs'),
-server=output.mkStaticFileServer()
+server=output.mkStaticFileServer(),
 //@todo public & private defaults (only sync public data with server)
-//tmp=console.log(truth),
-//{pre,post,state,update}=truth(util.clone(config.state))
+{pre,post,state,update}=truth(util.clone(config.state))
 //websocket server
 
 
+//console.log(server)
+chant(server,state,update)
 
 process.on('uncaughtException',function(err)//@todo eliminate if not debug
 {
