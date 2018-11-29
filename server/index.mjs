f696c5ce34585fb451bbe4ad3b5b4d579b5df1d5
Minifyre <minifyre@gmail.com>
2018-11-28 19:23:27 -0600

    +live silo recompilation
    The file watcher will watch the parent folder for changes and if a change is made to a file in src, then it's build file will be recompiled on the fly (there is a guard against overuse since Windows tells the filesystem a file changed multiple times).

--- server/index.mjs
+++ server/index.mjs
@@ -1,4 +1,6 @@
 import chant from '../node_modules/chant/server.mjs'
+import compile from '../node_modules/silo/compiler.mjs'
+import fs from 'fs'
 import silo from './input.mjs'
 import truth from '../node_modules/truth/truth.mjs'
 
@@ -17,4 +19,31 @@ process.on('uncaughtException',function(err)//@todo eliminate if not debug
 {
 	console.error(err)
 	console.log("Node NOT Exiting...")
+})
+
+//file watcher/compiler (beta)
+const watcher=
+{
+	delay:200,//prevents duplicate logs on windows
+	path:'./../../paper/',
+	timer:false
+}
+
+fs.watch(watcher.path,{recursive:true},function(type,path)
+{
+	const dirs=path.split('\\')
+
+	if(!dirs.includes('src')) return
+	
+	if(watcher.timer) clearTimeout(watcher.timer)
+
+	watcher.timer=setTimeout(async function()
+	{
+		watcher.timer=false
+
+		await compile(watcher.path+dirs.slice(0,-1).join('/')+'/')
+
+		console.log(path+' recompiled')
+
+	},watcher.delay)
 })
