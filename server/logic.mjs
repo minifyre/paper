257e4000ddf381fd856045d9010a8e6f7c8917bd
Minifyre <minifyre@gmail.com>
2018-12-08 17:29:41 -0600

    +basic login check

--- server/logic.mjs
+++ server/logic.mjs
@@ -8,4 +8,12 @@ logic.ext=x=>(x.split('.')||['']).pop().toLowerCase()
 
 logic.authPwdCheck=async (pwd,hash)=>await bcrypt.compare(pwd,hash)
 logic.authPwdHash=async (pwd,saltRounds=10)=>await bcrypt.hash(pwd,saltRounds)
+
+logic.authLogin=async function(db,username,pwd)
+{
+	const hash=db[username]
+	//user does not exist//@todo add random delay to make less obvious
+	return !hash?false:await logic.authPwdCheck(pwd,hash)
+}
+
 export default Object.assign(silo,{logic})
