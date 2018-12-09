687bd511adefb885ee977c4af257c55ce234edd2
Minifyre <minifyre@gmail.com>
2018-12-08 19:21:32 -0600

    +cookie utils

--- server/util.mjs
+++ server/util.mjs
@@ -13,4 +13,16 @@ util.callback2promise=function(func,...args)
 }
 util.clone=json=>JSON.parse(JSON.stringify(json))
 
+util.cookieParse=function(txt)
+{
+	return txt.split(';')
+		.map(keyVal=>keyVal.split('='))
+		.reduce((obj,[key,val])=>Object.assign(obj,{[key]:val}),{})
+}
+util.cookieStringify=function(obj)
+{
+	return Object.entries(obj)
+		.map(([key,val])=>key+'='+val)
+		.join(';')
+}
 export default Object.assign(silo,{util})
