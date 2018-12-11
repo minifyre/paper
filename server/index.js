464f6954c56ace473b4b24103414c27e61040e48
Minifyre <minifyre@gmail.com>
2018-12-10 19:58:28 -0600

    +init function
    Init sets up base state & then generates a server from it.

--- server/index.js
+++ server/index.js
@@ -3,21 +3,26 @@ import chant from '../node_modules/chant/server.mjs'
 import fs from 'fs'
 import truth from '../node_modules/truth/truth.mjs'
 
-import silo from '../node_modules/silo/index.js'
-
-const 
-{config,input,logic,output,util}=app,
-server=output.mkStaticFileServer(),
+const {config,input,logic,output,util}=app
 //@todo public & private defaults (only sync public data with server)
-{pre,post,state,update}=truth(util.clone(config.state))
-//websocket server
+// {pre,post,state,update}=truth(util.clone(config.state))
+// //websocket server
 
-
-//console.log(server)
-chant(server,state,update)
+// //console.log(server)
+// chant(server,state,update)
 
 process.on('uncaughtException',function(err)//@todo eliminate if not debug
 {
 	console.error(err)
 	console.log("Node NOT Exiting...")
 })
+
+;(async function init()
+{
+	const
+	users=await util.callback2promise(fs.readFile,'./private/db.json')
+	.then(txt=>JSON.parse(txt))
+	.catch(()=>console.error('private/db.json not found'))
+
+	output.mkStaticFileServer({file:{users,sessions:{}},view:{}})
+})()
